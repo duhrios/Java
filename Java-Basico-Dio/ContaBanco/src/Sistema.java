05492a9c7507788a78f28077e6f8186a812a1c1c
duhrios <eduardo123rios@gmail.com>
2024-09-12 13:58:23 -0300

    yes

--- Java-Basico-Dio/ContaBanco/src/Sistema.java
+++ Java-Basico-Dio/ContaBanco/src/Sistema.java
@@ -1,7 +1,28 @@
-public class Sistema {
-        int numero;
-        String agencia;
-        static String nomeCliente;
-        float Saldo;
+import java.util.Scanner;
 
+public class ContaTerminal {
+    int numero;
+    String agencia;
+    String nomeCliente;
+    float Saldo;
+
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+
+        ContaTerminal conta1 = new ContaTerminal();
+
+        System.out.println("Digite Seu nome: ");
+        conta1.nomeCliente = scanner.nextLine();
+
+        System.out.println("Digite a agência:");
+        conta1.agencia = scanner.nextLine();
+
+        System.out.println("Digite o numero da conta:");
+        conta1.numero = scanner.nextInt();
+
+        System.out.println("Digite o valor do deposito:");
+        conta1.Saldo = scanner.nextFloat();
+
+        System.out.printf("Olá %s, sua agência e conta são %s, %d. E seu saldo atual é: %.2f R$\n",conta1.nomeCliente, conta1.agencia, conta1.numero, conta1.Saldo);
+    }
 }
---
+++ Programas-simples/Prod.java
@@ -0,0 +1,26 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+  Scanner scanner = new Scanner (System.in);
+
+    int a = scanner.nextInt();
+    int b = scanner.nextInt();
+
+    int PROD = a - b;
+
+  System.out.println("PROD = " + PROD);
+
+
+
+
+
+
+
+
+    }
+
+}
